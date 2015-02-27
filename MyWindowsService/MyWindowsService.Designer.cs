9a401fc1b24309571f1744268aee2a265837fdcc
Steven Swenson <ctigeek@gmail.com>
2015-02-27 09:26:56 -0500

    Change windows service name to something generic.

--- MyWindowsService/MyWindowsService.Designer.cs
+++ MyWindowsService/MyWindowsService.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class MyWindowsService
     {
-        /// <summary> 
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +13,10 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            // 
-            // WebsitePerfAlertService
-            // 
-            this.ServiceName = "WebsitePerfAlertService";
-
+            //TODO: Change this name...
+            this.ServiceName = "MyWindowsService";
         }
-
-        #endregion
     }
 }
